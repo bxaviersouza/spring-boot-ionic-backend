817f21c9a6fd7e63f7c84d331576683f02f2969e
Bruno <b.xavier.souza@gmail.com>
2021-07-25 00:17:16 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/bruno/cursomc/services/Exception/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.bruno.cursomc.services.Exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
