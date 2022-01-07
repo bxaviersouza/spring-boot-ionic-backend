0c5aba8f91984107a7a1199fed2a56637c5fabc1
Bruno <b.xavier.souza@gmail.com>
2022-01-07 18:07:00 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/bruno/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.bruno.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bruno.cursomc.domain.Pedido;
+import com.bruno.cursomc.repositories.PedidoRepository;
+import com.bruno.cursomc.services.Exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
