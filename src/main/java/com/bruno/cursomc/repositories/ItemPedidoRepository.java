11dda9237bacc231246c36ae9d928d75732c13f4
Bruno <b.xavier.souza@gmail.com>
2022-01-07 17:43:41 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/bruno/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.bruno.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bruno.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
