2c3262a28469b543d1d4e2456c12070fbbd611a9
Bruno <b.xavier.souza@gmail.com>
2022-01-10 19:21:24 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/bruno/cursomc/services/CategoriaService.java
+++ src/main/java/com/bruno/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.bruno.cursomc.domain.Categoria;
+import com.bruno.cursomc.dto.CategoriaDTO;
 import com.bruno.cursomc.repositories.CategoriaRepository;
 import com.bruno.cursomc.services.Exception.DataIntegrityException;
 import com.bruno.cursomc.services.Exception.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
